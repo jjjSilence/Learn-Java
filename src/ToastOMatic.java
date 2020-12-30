dd40f82db9b5b39a4cbbc12d15297e5eba515b4b
jiangjiaojiao <17314236687@163.com>
2020-12-30 11:18:50 +0800

    并发Demo

---
+++ src/ToastOMatic.java
@@ -0,0 +1,163 @@
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class ToastOMatic {
+    public static void main(String[] args) throws InterruptedException {
+        ToastQueue dryQueue = new ToastQueue();
+        ToastQueue butteredQueue = new ToastQueue();
+        ToastQueue finishedQueue = new ToastQueue();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(new Toaster(dryQueue));
+        executorService.execute(new Butterer(dryQueue, butteredQueue));
+        executorService.execute(new Jammer(butteredQueue, finishedQueue));
+        executorService.execute(new Eater(finishedQueue));
+        TimeUnit.SECONDS.sleep(15);
+        executorService.shutdownNow();
+    }
+}
+
+class Toast {
+    public enum Status {DRY, BUTTERED, JAMMED}
+
+    private Status status = Status.DRY;
+    private final int id;
+
+    public Toast(int idn) {
+        id = idn;
+    }
+
+    public void butter() {
+        status = Status.BUTTERED;
+    }
+
+    public void jam() {
+        status = Status.JAMMED;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Toast{" +
+                "status=" + status +
+                ", id=" + id +
+                '}';
+    }
+}
+
+class ToastQueue extends LinkedBlockingQueue<Toast> {
+}
+
+class Toaster implements Runnable {
+    private ToastQueue toastQueue;
+    private int count = 0;
+    private Random random = new Random(47);
+
+    public Toaster(ToastQueue toastQueue) {
+        this.toastQueue = toastQueue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                TimeUnit.MILLISECONDS.sleep(2000 + random.nextInt(500));
+                Toast toast = new Toast(count++);
+                System.out.println(toast);
+                toastQueue.put(toast);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Toaster interrupted");
+        }
+        System.out.println("Toaster off");
+    }
+}
+
+class Butterer implements Runnable {
+    private ToastQueue dryQueue, butteredQueue;
+
+    public Butterer(ToastQueue dryQueue, ToastQueue buttered) {
+        this.dryQueue = dryQueue;
+        this.butteredQueue = buttered;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                TimeUnit.MILLISECONDS.sleep(2000);
+                Toast toast = dryQueue.take();
+                toast.butter();
+                System.out.println(toast);
+                butteredQueue.put(toast);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Butterer interrupted");
+        }
+        System.out.println("Butterer off");
+    }
+}
+
+class Jammer implements Runnable {
+    private ToastQueue butteredQueue, finishedQueue;
+
+    public Jammer(ToastQueue buttered, ToastQueue finished) {
+        this.butteredQueue = buttered;
+        this.finishedQueue = finished;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                TimeUnit.MILLISECONDS.sleep(2000);
+                Toast toast = butteredQueue.take();
+                toast.jam();
+                System.out.println(toast);
+                finishedQueue.put(toast);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Jammer interrupted");
+        }
+        System.out.println("Jammer off");
+    }
+}
+
+class Eater implements Runnable {
+    private ToastQueue finishedQueue;
+    private int counter = 0;
+
+    public Eater(ToastQueue finished) {
+        this.finishedQueue = finished;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                TimeUnit.MILLISECONDS.sleep(2000);
+                Toast toast = finishedQueue.take();
+                if (toast.getId() != counter++ ||
+                        toast.getStatus() != Toast.Status.JAMMED) {
+                    System.out.println(">>>>Error: " + toast);
+                    System.exit(1);
+                } else {
+                    System.out.println("Chomp! " + toast);
+                    System.out.println();
+                }
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Eater interrupted");
+        }
+        System.out.println("Eater off");
+    }
+}
