bd8516b998ffd31bcc242715a017cf4019569039
jiangjiaojiao <17314236687@163.com>
2021-01-04 11:20:59 +0800

    并发Demo

---
+++ src/ExchangerDemo.java
@@ -0,0 +1,8 @@
+import java.util.concurrent.Executors;
+
+public class ExchangerDemo {
+    static int size = 10;
+    static int delay = 5;
+    public static void main(String[] args) {
+    }
+}
---
+++ src/SemaphoreDemo.java
@@ -0,0 +1,154 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class SemaphoreDemo {
+    final static int SIZE = 5;
+
+    public static void main(String[] args) throws InterruptedException {
+        final Pool<Fat> pool = new Pool<Fat>(Fat.class, SIZE);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < SIZE; i++) {
+            executorService.execute(new CheckoutTask<Fat>(pool));
+        }
+        System.out.println("All CheckoutTasks created");
+        List<Fat> list = new ArrayList<Fat>();
+        for (int i = 0; i < SIZE; i++) {
+            Fat fat = pool.checkOut();
+            System.out.println(i + ": main() thread checked out ");
+            fat.operation();
+            list.add(fat);
+        }
+
+        Future<?> blocked = executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Fat fat = pool.checkOut();
+                    System.out.println("checked out " + fat);
+                } catch (InterruptedException e) {
+                    System.out.println("checkout() interrupted");
+                }
+            }
+        });
+
+        TimeUnit.SECONDS.sleep(2);
+        blocked.cancel(true);
+        System.out.println("Checking in objects in " + list);
+        for (Fat fat : list) {
+            pool.checkIn(fat);
+        }
+        for (Fat fat : list) {
+            pool.checkIn(fat);
+        }
+        TimeUnit.SECONDS.sleep(2);
+        executorService.shutdown();
+    }
+}
+
+class Fat {
+    private volatile double d;
+    private static int counter = 0;
+    private final int id = counter++;
+
+    public Fat() {
+        for (int i = 1; i < 10000; i++) {
+            d += (Math.PI + Math.E) / (double) i;
+        }
+    }
+
+    public void operation() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Fat{" +
+                "id=" + id +
+                '}';
+    }
+}
+
+class Pool<T> {
+    private int size;
+    private List<T> items = new ArrayList<T>();
+    private volatile boolean[] checkedOut;
+    private Semaphore available;
+
+    public Pool(Class<T> classObject, int size) {
+        this.size = size;
+        checkedOut = new boolean[size];
+        available = new Semaphore(size, true);
+        for (int i = 0; i < size; ++i) {
+            try {
+                items.add(classObject.newInstance());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public T checkOut() throws InterruptedException {
+        available.acquire();
+        return getItem();
+    }
+
+    public void checkIn(T x) {
+        if (releaseItem(x)) {
+            available.release();
+        }
+    }
+
+    private synchronized T getItem() {
+        for (int i = 0; i < size; ++i) {
+            if (!checkedOut[i]) {
+                checkedOut[i] = true;
+                T t = items.get(i);
+//                System.out.println("-------------: " + t);
+                return t;
+            }
+        }
+        return null;
+    }
+
+    private synchronized boolean releaseItem(T item) {
+        int index = items.indexOf(item);
+        if (index == -1) return false;
+        if (checkedOut[index]) {
+            checkedOut[index] = false;
+            return true;
+        }
+        return false;
+    }
+}
+
+class CheckoutTask<T> implements Runnable {
+
+    private static int counter = 0;
+    private static int id = counter++;
+    private Pool<T> pool;
+
+    public CheckoutTask(Pool<T> pool) {
+        this.pool = pool;
+    }
+
+    @Override
+    public void run() {
+        try {
+            T item = pool.checkOut();
+            System.out.println(this + "checked out " + item);
+            TimeUnit.SECONDS.sleep(1);
+            System.out.println(this + "checking in " + item);
+            pool.checkIn(item);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CheckoutTask{" +
+                "id=" + id +
+                '}';
+    }
+}
